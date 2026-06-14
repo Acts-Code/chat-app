4621d7e976ec0065a59548980b64573e37776a83
Acts-Code <laxmanpandey2070@gmail.com>
2026-06-14 08:03:45 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
 
+app.use(cors());
+
 const io = new Server(server, {
-    cors: { origin: "*" }
+    cors: {
+        origin: "*"
+    }
 });
 
-io.on("connection", (socket) => {
-    console.log("User connected");
+let messages = [];
 
-    socket.on("message", (msg) => {
-        io.emit("message", msg);
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    // send old messages
+    socket.emit("load_messages", messages);
+
+    // receive new message
+    socket.on("send_message", (msg) => {
+        messages.push(msg);
+        io.emit("new_message", msg); // send to everyone instantly
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected");
+        console.log("User disconnected:", socket.id);
     });
 });
 
-app.get("/", (req, res) => {
-    res.send("Actor Chat Server Running 🚀");
-});
-
-server.listen(process.env.PORT || 3000, () => {
+server.listen(3000, () => {
     console.log("Server running");
 });
